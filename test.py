23f002ebcf644ba595e432ce8c3b352985217635
Vincenzo Timmel <vincenzo.timmel@gmail.com>
2022-11-17 10:02:43 +0100

    Updated tests

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import ducc0.wgridder as ng
 try:
     import wagg as wg
-except ImportError: # If no GPU is found, an ImportError is raised, which is OK. 
+except ImportError as IE: # If no GPU is found, an ImportError is raised, which is OK. 
     pass
